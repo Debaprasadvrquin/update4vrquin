0b63602b0e8baae23b6163b42d6ebbaa5fa7dab7
Debaprasadvrquin <126150966+Debaprasadvrquin@users.noreply.github.com>
2023-07-05 12:49:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 unclutter -idle 5 -root &
 sudo rfkill unblock wlan
 sudo python3 /home/pi/captive-portal/captive_portal.py &
-cd $HOME/player2
+cd /home/pi/player2
 #. /home/pi/.bash_profile
-mkdir -p $HOME/.config/gtk-3.0 #gtk-3.0 directory not found in .config dir
-cp shell-scripts/gtk-3.0/gtk.css $HOME/.config/gtk-3.0
+mkdir -p /home/pi/.config/gtk-3.0 #gtk-3.0 directory not found in .config dir
+cp shell-scripts/gtk-3.0/gtk.css /home/pi/.config/gtk-3.0
 python remove.py &
 python restart.py &
 python thumbnails.py &
